5746988d79bab8b15c3997f16e2310ab415a8ba2
Tim Verberne <timverbe@gmail.com>
2023-02-03 22:23:58 +0100

    project setup

---
+++ Source/GlobalGameJam2023/GlobalGameJam2023.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GlobalGameJam2023.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GlobalGameJam2023, "GlobalGameJam2023" );
+ 
